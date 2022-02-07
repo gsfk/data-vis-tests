f44a73867b5dfd701bb21d495e498fa5d730de95
Gordon Krieger <gordon.krieger@gmail.com>
2022-02-07 17:35:49 -0500

    lollipop and scatter

---
+++ src/components/Lollipop.js
@@ -0,0 +1,8 @@
+import React from "react"
+// mutation mapper?
+
+const Lollipop = () => {
+  return null;
+}
+
+export default Lollipop;
---
+++ src/components/Scatterplot.js
@@ -0,0 +1,8 @@
+import React from "react"
+// victory scatter?
+
+const Scatterplot = () => {
+  return null;
+}
+
+export default Scatterplot;
