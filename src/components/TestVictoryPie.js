ce8084da0b9d95599fa30c37fcec6d6cefbd375c
Gordon Krieger <gordon.krieger@gmail.com>
2022-03-02 10:02:18 -0500

    basic victory pie

--- src/components/TestVictoryPie.js
+++ src/components/TestVictoryPie.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { VictoryPie, VictoryTooltip } from "victory";
+import {COLOURS} from "../constants"
+
+const wrapperStyle = {width: "200px", border: "1px solid #dddddd", borderRadius: "4px"}
+
 
 const TestVictoryPie = ({ title, data, height }) => {
-  return <h3>victory pie goes here</h3>;
+  return (
+    <div style={wrapperStyle}>
+    <VictoryPie
+      data={data}
+      width={height}
+      height={height}
+      labelComponent={<VictoryTooltip/>}
+      colorScale={COLOURS}
+      style={{
+        labels: {
+          fontSize: 6,
+        },
+      }}
+    />
+    </div>
+  );
 };
 
 export default TestVictoryPie;
